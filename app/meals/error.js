8b71afec33ef9b7967b97f4c3609e45700ffe85f
Tareq-Adel <tareq212adel@gmail.com>
2025-01-26 22:43:32 +0200

    Adding error handling and not-found page

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error(errorObj) {
+	return (
+		<main className='error'>
+			<h1>An Error Accored!!!</h1>
+			<p>Feald to feach a meal data. Please try again later.</p>
+		</main>
+	);
+}
